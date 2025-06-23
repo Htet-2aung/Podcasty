0188a92e63a7d2f698d3e2599dc9364ba0f5e3ed
Htet Aung <kienbrown212001@gmail.com>
2025-06-23 16:03:13 +0700

    Update WelcomePage.tsx

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { useUser } from '../context/UserProvider';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const WelcomePage = () => {
   const { session } = useUser();
@@ -12,7 +13,13 @@ const WelcomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-background text-text-main p-8 text-center">
-      <img src="/welcome_dark.png" alt="Podcast illustration" className="w-auto h-auto max-w-xs max-h-80 mb-8" />
+      
+      <DotLottieReact
+      src="https://lottie.host/79d8548c-3630-4f9e-9194-66aa466cc7c3/M1wQDyDKc5.lottie"
+      loop
+      autoplay
+    />
+      
       <h1 className="text-4xl font-bold mb-4">Say it with Podcast</h1>
       <p className="text-lg text-text-secondary mb-8 max-w-sm">
         Discover, listen, and subscribe to your favorite podcasts from around the world.
