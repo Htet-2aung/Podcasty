dbcc02a783b1f94a1e60d6a12fa3cfdec0e0d5c5
Htet Aung <kienbrown212001@gmail.com>
2025-06-23 18:08:20 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,100 @@
+// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from './context/ThemeProvider.tsx';
+import { AudioProvider } from './context/AudioProvider.tsx';
+import { UserProvider } from './context/UserProvider';
+
+// Import Pages and Components
+import App from './App.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
+import WelcomePage from './pages/WelcomePage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import SignupPage from './pages/SignupPage.tsx';
+import HomePage from './pages/HomePage.tsx';
+import SearchPage from './pages/SearchPage.tsx';
+import PodcastPage from './pages/PodcastPage.tsx';
+import LibraryPage from './pages/LibraryPage.tsx';
+import ProfilePage from './pages/ProfilePage.tsx';
+import CategoryPage from './pages/CategoryPage.tsx';
+import ProtectedRoute from './components/ProtectedRoute.tsx';
+
 import './index.css';
 
-function App() {
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minHeight: '100vh',
-      backgroundColor: '#121826',
-      color: '#e6f1ff',
-      fontFamily: 'sans-serif'
-    }}>
-      <h1 style={{ fontSize: '3rem' }}>Test Page</h1>
-      <p style={{ marginTop: '1rem' }}>If you can see this message, the basic deployment is working correctly.</p>
-    </div>
-  );
-}
+// Define the application's routes
+const router = createBrowserRouter([
+  // Public routes
+  {
+    path: '/welcome',
+    element: <WelcomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />,
+    errorElement: <ErrorPage />,
+  },
+
+  // Protected routes that require a user to be logged in
+  {
+    path: '/',
+    element: <ProtectedRoute />, // This component guards all children
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />, // App renders the main layout (sidebar, etc.)
+        children: [
+          {
+            path: '/',
+            element: <HomePage />,
+          },
+          {
+            path: 'search',
+            element: <SearchPage />,
+          },
+          {
+            path: 'podcast/:podcastId',
+            element: <PodcastPage />,
+          },
+          {
+            path: 'library',
+            element: <LibraryPage />,
+          },
+          {
+            path: 'profile',
+            element: <ProfilePage />,
+          },
+          {
+            path: 'category/:genreId',
+            element: <CategoryPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <ThemeProvider>
+        <UserProvider>
+          <AudioProvider>
+            <RouterProvider router={router} />
+          </AudioProvider>
+        </UserProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
+
+
